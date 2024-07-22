f8aaea0e5e76ef17f13e594b256665080f2940f8
HyAdrian <hygorat@gmail.com>
2024-07-22 14:21:17 -0300

    Problemas no lugar onde dei commit, ocorreu pq eu tinha duas copias do projeto

--- back/contas-service/repository/write/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.contasservice.repository.write;
-
-import com.example.contasservice.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-    Cliente getByCpf(String cpf);
-}
--- back/contas-service/repository/write/MovimentacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.contasservice.repository.write;
-
-import com.example.contasservice.model.Movimentacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
-}
